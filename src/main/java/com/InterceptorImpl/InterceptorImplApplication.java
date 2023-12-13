187943070c6d2a42d4f2e0e4b1e07fe9b3bae0c8
sk obedullah <obedullahsk@gmail.com>
2023-12-14 01:19:29 +0530

    added skeliton for implementation for a interceptor demo

---
+++ src/main/java/com/InterceptorImpl/InterceptorImplApplication.java
@@ -0,0 +1,13 @@
+package com.InterceptorImpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterceptorImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterceptorImplApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/InterceptorImpl/InterceptorImplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InterceptorImpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterceptorImplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
